232288c9f04373a60756538107389209cfa7af0e
Yoshi101-web <yoshi.dosanko@gmail.com>
2020-07-22 03:09:03 +0900

    add fontawesome

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -6,9 +6,13 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Web</title>
     <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
 </head>
 <body>
     <div id="app">
+    <i class="fas fa-microphone"></i>
+        <i class="fas fa-microphone-slash"></i>
+        <i class="fas fa-sign-out-alt"></i>
         <web-speech></web-speech>
     </div>
     <script src="{{ asset('js/app.js') }}" defer></script>
