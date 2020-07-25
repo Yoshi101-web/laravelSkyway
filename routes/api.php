d744c882915d7ed88272547df9ec8a1fbc8f2ebb
Yoshi101-web <yoshi.dosanko@gmail.com>
2020-07-25 23:11:58 +0900

    change the directory of the ChatController

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/contents', 'ChatController@store');
+Route::post('/contents', 'Api\ChatController@store');
