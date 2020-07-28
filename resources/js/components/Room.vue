aafeef91a6a45ae9dc70f51ec7bd86c9218caec2
Yoshi101-web <yoshi.dosanko@gmail.com>
2020-07-29 02:22:13 +0900

    Create Room.vue

---
+++ resources/js/components/Room.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <SkyWay></SkyWay>
+        <WebSpeech></WebSpeech>
+    </div>
+</template>
+
+<script>
+import SkyWay from "./SkyWay";
+import WebSpeech from "./WebSpeech";
+export default {
+    components: {
+        SkyWay,
+        WebSpeech
+    },
+    mounted() {
+        console.log('chat')
+    }
+}
+</script>
